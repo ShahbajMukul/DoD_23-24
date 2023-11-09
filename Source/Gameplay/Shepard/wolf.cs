dc53eac882bea327a4a4add995b4018ee624aeb4
Maheel Luqman <maheelllc@gmail.com>
2023-11-09 14:38:20 -0500

    Create wolf.cs

---
+++ Source/Gameplay/Shepard/wolf.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Includes
+using System.Xml.Linq;
+using System.Threading;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+#endregion
+
+namespace DoD_23_24
+{
+    public class Wolf : Basic2D
+    {
+        float speed = 45f;
+        public Wolf(string PATH, Vector2 POS, Vector2 DIMS, bool shouldScale) : base(PATH, POS, DIMS, shouldScale);
+           
+    }
+{
+}
+}
